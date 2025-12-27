c0c26997b8f3b05eb58c018e3945311cb75d44e8
maheshkumawat274 <maheshkumar006376@gmail.com>
2025-12-27 20:32:06 +0530

    done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const PodcastHeader: React.FC = () => {
             <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-emerald-400 rounded" />
           </span>
 
-          
+                  
 
           <a
             href="#exclusive"
@@ -36,7 +36,7 @@ const PodcastHeader: React.FC = () => {
             Exclusive Podcasts
           </a>
 
-          
+                  
 
           <a
             href="https://forms.gle/WwiqSPvndCYQQNUa8"
